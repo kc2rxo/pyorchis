67a5d3a8f32e08c0467cbc65a68d7129935477f6
Adam Wiethuechter <kc2rxo@gmail.com>
2026-09-11 23:45:52 -0400

    refactor: new suite id management
    Adds HiAlgorithm enum and explicitly marks old SuiteId to be for HIP (both HITs and DETs).

--- src/orchis/constant.py
+++ src/orchis/constant.py
@@ -21,21 +21,73 @@ class Prefix(Enum):
             raise ValueError("unknown IPv6 prefix")
 
 
-class SuiteId(IntEnum):
+class HiAlgorithm(IntEnum):
+    RESERVED = 0
+    NULL_ENCRYPT = 1
+    # Unassigned = 2
+    DSA = 3
+    # Unassigned = 4
+    RSA = 5
+    # Unassigned = 6
+    ECDSA = 7
+    # Unassigned = 8
+    ECDSA_LOW = 9
+    # Unassigned = 10
+    # Unassigned = 11
+    # Unassigned = 12
+    EDDSA = 13
+    # Unassigned = 14 - 65535
+
+
+class HipSuiteId(IntEnum):
     RESERVED = 0
 
-    # 1-15 are Basic HIT Suite IDs (upper nibble of 8-bit HIT Suite ID of RFC7401)
-    RSA_DSA_SHA256 = 1
-    ECDSA_SHA384 = 2
-    # ECDSA_LOW_SHA1 = 3  # not supported due to missing curve
-    # Unassigned 4
-    EDDSA_CSHAKE128 = 5
+    # 0x01 - 0x0F: Extended HIT Suite ID Mapping; draft-atw-orchid-v3, Appendix C
+    # Unassigned = 1
+    ECDSA_P256_SHA384 = 2
+    ECDSA_LOW_SECP160R1_SHA1 = 3  # not supported, missing curve
+    # Unassigned = 4
+    EDDSA_25519_CSHAKE128 = 5
+    # Unassigned = 6 - 15
 
-    # 16 skipped to align with lower nibble of 8-bit HIT Suite ID
-    # 17-31 are Extended HIT Suite IDs (lower nibble of 8-bit HIT Suite ID of RFC7401)
-    # Unassigned 17-31
+    # 0x[1-F]0: Basic HIT Suite ID Mapping; draft-atw-orchid-v3, Appendix C
+    RSA_DSA_SHA256 = 16
+    ECDSA_SHA384 = 32
+    ECDSA_LOW_SHA1 = 48  # not supported, missing curve
+    # Unassigned = 64
+    EDDSA_CSHAKE128 = 80
+    # Unassigned = 96
+    # Unassigned = 112
+    # Unassigned = 128
+    # Unassigned = 144
+    # Unassigned = 160
+    # Unassigned = 176
+    # Unassigned = 192
+    # Unassigned = 208
+    # Unassigned = 224
+    # Unassigned = 240
 
-    # 32+ are HHIT Suite IDs defined by RFC9374
-    # Unassigned 32-253
+    # DET Suite ID Blocks; draft-atw-orchid-v3, Appendix C
+    # 0x11 - 0x1F
+    # 0x21 - 0x2F
+    ECDSA_P384_SHA384 = 33
+    # ECDSA_P256_CSHAKE128 = 34
+    # ECDSA_P384_CSHAKE256 = 35
+    # 0x31 - 0x3F
+    # 0x41 - 0x4F
+    # 0x51 - 0x5F
+    EDDSA_25519PH_CSHAKE128 = 81
+    EDDSA_448_CSHAKE256 = 82
+    EDDSA_448PH_CSHAKE256 = 83
+    # 0x61 - 0x6F
+    # 0x71 - 0x7F
+    # 0x81 - 0x8F
+    # 0x91 - 0x9F
+    # 0xA1 - 0xAF
+    # 0xB1 - 0xBF
+    # 0xC1 - 0xCF
+    # 0xD1 - 0xDF
+    # 0xE1 - 0xEF
+    # 0xF1 - 0xFF
     PRIVATE_USE_1 = 254
     PRIVATE_USE_2 = 255
